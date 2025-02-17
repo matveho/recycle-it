4635ad50476db29000f72918108ca475ce136bae
mat <matvey.okoneshnikov@gmail.com>
2025-02-17 10:49:57 -0700

    Remove unused

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import Header from "@/app/components/Header"
 import MainContent from "@/app/components/MainContent";
-import Footer from "@/app/components/Footer"
-
 
 export default function Page() {
     return (
